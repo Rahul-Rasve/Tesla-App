bb9c1e74cc332727aebe3f83110d6ce129e47dc5
Rahul Rasve <rahulrasve712@gmail.com>
2024-10-08 17:32:03 +0530

    layout configured (#1)

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.carContainer}>
+				<ImageBackground
+					source={require("./assets/images/ModelS.jpeg")}
+					style={styles.image}
+				/>
+
+				<View style={styles.headerSection}>
+					<Text style={styles.title}>Model S</Text>
+
+					<Text style={styles.subtitle}>Starting at $69,420</Text>
+				</View>
+			</View>
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	carContainer: {
+		width: "100%",
+		height: "100%",
+	},
+
+	image: {
+		width: "100%",
+		height: "100%",
+		resizeMode: "contain",
+    position: "absolute",
+	},
+
+	headerSection: {
+		marginTop: "25%",
+		width: "100%",
+		alignItems: "center",
+	},
+
+	title: {
+		fontSize: 40,
+		fontWeight: "600",
+	},
+
+	subtitle: {
+		fontSize: 16,
+		color: "#5c5e62",
+		marginTop: 5,
+	},
 });
